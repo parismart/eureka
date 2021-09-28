1b1ca9d883ff6eeb9fc1be6cce72d8dbe3180961
Paris <parismrt@gmail.com>
2021-09-28 11:44:05 +0200

    Update btc-price.py

--- btc-price.py
+++ btc-price.py
@@ -1,8 +1,9 @@
 import requests
 import json
+import os
 import time
 while True:
-	t = requests.get(https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd).txt
+	t = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd').txt
 	t = json.loads(t)
 	price = int(t["bitcoin"]["usd"])
 
