f2aed75b5460da721623db9898d91a9042088a1d
Paris <parismrt@gmail.com>
2021-09-28 11:15:53 +0200

    Create btc-price.py

---
+++ btc-price.py
@@ -0,0 +1,10 @@
+import requests
+import json
+import time
+while True:
+	t = requests.get(https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd).txt
+	t = json.loads(t)
+	price = int(t["bitcoin"]["usd"])
+
+	os.system(f"notify-send -t 10000 \"El precio de BTC es {price}\"")
+	time.sleep(60)
