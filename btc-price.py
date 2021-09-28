f6a491b2539b06804a0135039d01f900494c433f
Paris <parismrt@gmail.com>
2021-09-28 12:35:16 +0200

    Update btc-price.py

--- btc-price.py
+++ btc-price.py
@@ -1,11 +1,13 @@
 import requests
 import json
-import os
 import time
-while True:
-	t = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd').txt
-	t = json.loads(t)
-	price = int(t["bitcoin"]["usd"])
-
-	os.system(f"notify-send -t 10000 \"El precio de BTC es {price}\"")
-	time.sleep(60)
+data = requests.get('https://api.coingecko.com/api/v3/search/trending').text
+parse = json.loads(data)
+print(json.dumps(parse, indent=4, sort_keys=True))
+#while True:
+	#t = requests.get('https://api.coingecko.com/api/v3/coins/bitcoin/history?date=25-09-2021&localization=es').text
+	#t = json.loads(t)
+	#price = int(t["bitcoin"]["usd"])
+	#print("El Historico de BTC es", price)
+	#print(t)
+	#time.sleep(5)
